e23aeafd11de737573ae685254696c3f674c4d43
nurudeen <ajayinurudeen998@gmail.com>
2017-07-23 22:54:49 +0100

    changes

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -23,9 +23,9 @@
     				     <span class="input-group-btn">
     				       <a href="/search" class="btn btn-default" id="discover" type="button">Search</a>
     				     </span>
-    					</div> <br>
+    					</div> 
     					<!-- /input-group -->
-    					<center class="text-center" style="font-size: 20px; "><span style="color: #d9534f">...discover nigeria authentic products and services place. </span>  <br> <br></center>
+    					<center class="text-center" style="font-size: 14px; "><span style="color: #d9534f">...discover nigeria authentic products and services place. </span>  <br> <br></center>
     				</div>
     			</div>
     		</div>
@@ -59,19 +59,19 @@
                     </div>
                 </div> <br/>
                    
-            </div>
-            <div class="row">
+            </div> <br> 
+                    <center> <b style="color:#5cb85c;"> Connect with us: </b> </center>            
+            <div class="row"> 
                 <div class="col-lg-8 col-lg-offset-4 col-xs-8 col-xs-offset-3">
-                    
                     <div class="col-lg-2 col-xs-3">
-                               <img src="{{asset('images/f.PNG')}}" style=" height:30px;"  class="img img-responsive img-rounded">                 
+                               <a href="https://www.facebook.com/nairaproducts" target="_blank"> <img src="{{asset('images/f.PNG')}}" style=" height:30px;"  class="img img-responsive img-rounded"> </a>              
                     </div>
                     <div class="col-lg-2 col-xs-3">
-                               <img src="{{asset('images/i.jpg')}}" style=" height:30px;" class="img img-responsive img-rounded">                 
+                               <a href="https://www.twitter.com/nairaproducts" target="_blank"> <img src="{{asset('images/i.jpg')}}" style=" height:30px;" class="img img-responsive img-rounded"> </a>               
                                                 
                     </div>
                     <div class="col-lg-2 col-xs-3">
-                               <img src="{{asset('images/t.png')}}" style=" height:30px;"  class="img img-responsive img-rounded">
+                             <a href="">  <img src="{{asset('images/t.png')}}" style=" height:30px;"  class="img img-responsive img-rounded"> </a>
                     </div>
                      {{--  <div class="col-lg-2 col-xs-3">  --}}
                                {{--  <img src="{{asset('images/splendy.jpg')}}" class="img img-responsive img-rounded">                   --}}
